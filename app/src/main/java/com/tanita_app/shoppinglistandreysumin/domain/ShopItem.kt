fdd969c7d0be086ccd4acd44d7e0564d3484e9c4
Tanita <hruzina_t@mail.ru>
2022-09-22 12:55:06 +0300

    Создан слой Data - Создали реализацию интерфейса ShopListRepository

--- app/src/main/java/com/tanita_app/shoppinglistandreysumin/domain/ShopItem.kt
+++ app/src/main/java/com/tanita_app/shoppinglistandreysumin/domain/ShopItem.kt
@@ -1,10 +1,15 @@
 package com.tanita_app.shoppinglistandreysumin.domain
 
 data class ShopItem(
-    val id: Int,
     val name: String,
     val count: Int,
-    val enabled: Boolean
-)
+    val enabled: Boolean,
+    var id: Int = UNDEFINED_ID
+) {
+
+    companion object {
+        const val UNDEFINED_ID = -1
+    }
+}
 
 
